f6473bbee8a00a955a877d92a6d7e5c114d9eb99
juanSe756 <juansebastiangilborda@gmail.com>
2023-10-26 23:24:58 -0500

    lombok agregado,pendiente crear entitymanager

--- src/main/java/com/artesaniasbetty/App.java
+++ src/main/java/com/artesaniasbetty/App.java
@@ -2,10 +2,14 @@ package com.artesaniasbetty;
 
 import com.artesaniasbetty.gui.StartFrame;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class App implements ActionListener {
+    private static EntityManagerFactory ENTITY_MANAGER_FACTORY = Persistence
+            .createEntityManagerFactory("artesaniasbetty");
     StartFrame sf;
     public App() {
         sf = new StartFrame(this);
--- src/main/java/com/artesaniasbetty/model/Producto.java
+++ src/main/java/com/artesaniasbetty/model/Producto.java
@@ -1,9 +1,27 @@
 package com.artesaniasbetty.model;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Table(name = "Productos")
 public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "nombre")
     private String nombre;
+    @Column(name = "precio")
     private double precio;
+    @Column(name = "cant_disp")
     private int cant_disp;
+    @Column(name = "desc")
     private String desc;
 
 }
