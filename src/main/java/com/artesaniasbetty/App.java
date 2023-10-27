5a45de854d94a34c9d4529eb631e53ebf43a08e5
juanSe756 <juansebastiangilborda@gmail.com>
2023-10-26 22:16:30 -0500

    maven structured and java swing login created

---
+++ src/main/java/com/artesaniasbetty/App.java
@@ -0,0 +1,23 @@
+package com.artesaniasbetty;
+
+import com.artesaniasbetty.gui.StartFrame;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App implements ActionListener {
+    StartFrame sf;
+    public App() {
+        sf = new StartFrame(this);
+    }
+    public static void main(String[] args) {
+        new App();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (e.getActionCommand()){
+            case "SEE_PASSWORD":    sf.managePasswordButton();
+        }
+    }
+}
