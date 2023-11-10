834b4fdaf419ccc3dfa579be423838efebbef2a1
juanSe756 <juansebastiangilborda@gmail.com>
2023-11-09 22:13:36 -0500

    getBill and getTotalCostByBill methods made it.

--- src/main/java/com/artesaniasbetty/controllers/VentaController.java
+++ src/main/java/com/artesaniasbetty/controllers/VentaController.java
@@ -2,12 +2,23 @@ package com.artesaniasbetty.controllers;
 
 import com.artesaniasbetty.model.*;
 import jakarta.persistence.*;
+import lombok.Getter;
 
 import java.sql.Timestamp;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
+@Getter
 public class VentaController {
+    private HashMap<Integer, Integer> productosVenta;
+
+    public List<Venta> getSales() {
+        try (EntityManager em = EntityMF.getInstance().createEntityManager()) {
+            return em.createQuery("SELECT v FROM Venta v", Venta.class).getResultList();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
     public String recordSale(String desc, Timestamp fecha_regist_venta, int idUsuario, HashMap<Integer, Integer> productosVenta) {
         ProductoController productoController = new ProductoController();
         try (EntityManager em = EntityMF.getInstance().createEntityManager()) {
@@ -16,7 +27,7 @@ public class VentaController {
             Venta venta = new Venta(desc, fecha_regist_venta, usuario);
             em.persist(venta);
             for (Integer producto : productosVenta.keySet()) {
-                DetalleVenta detalleVenta = new DetalleVenta(em.find(Producto.class, producto),venta, productosVenta.get(producto));
+                DetalleVenta detalleVenta = new DetalleVenta(em.find(Producto.class, producto), venta, productosVenta.get(producto));
                 productoController.decrementStock(producto, productosVenta.get(producto));
                 em.persist(detalleVenta);
             }
@@ -24,6 +35,28 @@ public class VentaController {
             return "Sale recorded";
         } catch (Exception e) {
             return "Error recording sale";
+        }
+    }
+
+    public double getTotalCostByBill(List<StringBuilder> bill) {
+        double total = 0;
+        for (StringBuilder stringBuilder : bill) {
+            total += Double.parseDouble(stringBuilder.substring(stringBuilder.lastIndexOf(" ") + 1));
+        }
+        return total;
+    }
+
+    public List<StringBuilder> getBill() {
+        List<StringBuilder> bill = new ArrayList<>();
+        try (EntityManager em = EntityMF.getInstance().createEntityManager()) {
+//            cant, nombre, precio, subtotal separados por espacios
+            for (Integer producto : productosVenta.keySet()) {
+                Producto p = em.find(Producto.class, producto);
+                bill.add(new StringBuilder().append(productosVenta.get(producto)).append(" ").append(p.getNombre()).append(" ").append(p.getPrecio()).append(" ").append(p.getPrecio() * productosVenta.get(producto)));
+            }
+            return bill;
+        } catch (Exception e) {
+            return null;
         }
     }
 }
