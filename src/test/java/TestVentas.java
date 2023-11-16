ec0657e6b3779e5dae8bd7579d71ec5c43a75067
juanSe756 <juansebastiangilborda@gmail.com>
2023-11-15 23:42:33 -0500

    tested getIncomeThisMonth

--- src/test/java/TestVentas.java
+++ src/test/java/TestVentas.java
@@ -9,16 +9,16 @@ import java.util.List;
 
 public class TestVentas {
     public static void main(String[] args) {
-        HashMap<Integer, Integer> productos = new HashMap<>();
-        productos.put(1, 2);
-        productos.put(2, 2);
-        productos.put(3, 2);
-        String sale = new VentaDAO().recordSale("Venta comun de algunos productos", 1, productos);
+//        HashMap<Integer, Integer> productos = new HashMap<>();
+//        productos.put(11, 4);
+//        productos.put(2, 2);
+//        productos.put(8, 6);
+//        String sale = new VentaDAO().recordSale("Venta de valor para la empresa", 1, productos);
 //        List<DetalleVenta> x = new VentaDAO().getDetalleFromVenta(2);
 //        List<Venta> x = new VentaDAO().getSalesThisMonth();
-//        double x = new VentaDAO().getIncomeThisMonth();
+        double x = new VentaDAO().getIncomeThisMonth();
 //        double x = new VentaDAO().getCostOfProducts(productos);
-        System.out.println(sale);
+        System.out.println(x);
 //        for (Venta venta : x) {
 //            System.out.println(venta.toString());
 //        }
